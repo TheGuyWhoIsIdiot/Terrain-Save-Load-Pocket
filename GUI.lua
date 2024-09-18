86f023734deb39ddb8bc557c57b0618a8afc99f1
TheGuyWhoIsIdiot <157973030+TheGuyWhoIsIdiot@users.noreply.github.com>
2024-09-18 18:48:21 -0300

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -1,1 +1,11 @@
+-- GUI instructions, it will create a gui ordered, and then return it to the script.
+-- made by meditext/TGWII
 
+-- ROOT gui
+local GUI = Instance.new("ScreenGui")
+GUI.Name = "TerrainSaveLoadGui"
+-- APP frame
+local App = Instance.new("Frame")
+App.Name = "App"
+App.Position = UDim2.new(0.5, -100, 0, 10)
+App.Size = UDim.new(0, 200,0, 96)
